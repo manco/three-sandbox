7781654d44a89e9001bb5f3562e9acfb99dc2904
Krzysztof Mozejko <k.mozejko@ocado.com>
2018-08-13 21:22:26 +0200

    loader.ts fix

--- src/utils/loader.ts
+++ src/utils/loader.ts
@@ -1,17 +1,18 @@
 import 'three/OBJLoader';
+import {Mesh, Object3D} from "three";
+
 const noop = (xhr) => {};
 export class PromisingLoader {
-    constructor() {
-        this.loader = new THREE.OBJLoader();
-    }
+    // @ts-ignore
+    private readonly loader = new THREE.OBJLoader();
     loadSingleMesh(url) {
-        return new Promise(
+        return new Promise<Object3D>(
             (resolve) => this.loader.load(url, resolve, noop, noop)
-        ).then(obj => {
+        ).then((obj:Object3D) => {
             if (obj.children.length > 1) {
                 console.warn(`loadSingleMesh: ${url} resolved to group of meshes: ${obj.children}`)
             }
-            return obj.children[0]
+            return obj.children[0] as Mesh
         });
     }
 }
