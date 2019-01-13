f69484a443f7e7dd42e8c95ac49d31c192470164
Krzysztof Mozejko <k.mozejko@ocado.com>
2019-01-13 13:07:38 +0100

    remove unused controls buttons code

--- src/controller/controls.ts
+++ src/controller/controls.ts
@@ -3,8 +3,6 @@ import {OrthographicCamera} from "three";
 import {OrbitControls} from "../utils/OrbitControls";
 
 export class Controls {
-    private static readonly RotateStep = Math.PI * 0.05;
-    private static readonly PanStep = 50;
 
     private readonly orbitControls: OrbitControls;
     constructor(private readonly camera: OrthographicCamera, canvas: HTMLCanvasElement) {
@@ -33,45 +31,6 @@ export class Controls {
         const newZoom = this.camera.zoom + delta;
         this.camera.zoom = Math.min(10, Math.max(0, newZoom));
         this.camera.updateProjectionMatrix();
-    }
-
-    panLeft() {
-        this.panHorizontally(Controls.PanStep);
-    }
-
-    panRight() {
-        this.panHorizontally(-Controls.PanStep);
-    }
-
-    private panHorizontally(deltaX: number) {
-        this.orbitControls.pan(deltaX, 0);
-        this.orbitControls.update();
-    }
-
-    rotateLeft() :void {
-        this.rotateHorizontally(Controls.RotateStep);
-    }
-
-    rotateRight() :void {
-        this.rotateHorizontally(-Controls.RotateStep);
-    }
-
-    private rotateHorizontally(angle: number) {
-        this.orbitControls.rotateLeft(angle);
-        this.orbitControls.update();
-    }
-
-    rotateUp() :void {
-        this.rotateVertically(Controls.RotateStep);
-    }
-
-    rotateDown() :void {
-        this.rotateVertically(-Controls.RotateStep);
-    }
-
-    private rotateVertically(angle: number) {
-        this.orbitControls.rotateUp(angle);
-        this.orbitControls.update();
     }
 
     reset() {
--- src/view/controlsPanel.ts
+++ src/view/controlsPanel.ts
@@ -11,24 +11,12 @@ export class ControlsPanel {
         const buttonZoomIn = panel.querySelector('button[id=\"zoomin\"]');
         const buttonZoomOut = panel.querySelector('button[id=\"zoomout\"]');
         const buttonCenter = panel.querySelector('button[id=\"center\"]');
-        const buttonPanLeft = panel.querySelector('button[id=\"panleft\"]');
         const buttonModePan = panel.querySelector('button[id=\"modepan\"]');
-        const buttonPanRight = panel.querySelector('button[id=\"panright\"]');
-        const buttonRotateLeft = panel.querySelector('button[id=\"rotateleft\"]');
-        const buttonRotateRight = panel.querySelector('button[id=\"rotateright\"]');
-        const buttonRotateUp = panel.querySelector('button[id=\"rotateup\"]');
-        const buttonRotateDown = panel.querySelector('button[id=\"rotatedown\"]');
         const buttonModeRotate = panel.querySelector('button[id=\"moderotate\"]');
 
         Events.onClick(buttonZoomIn, () => controls.zoomIn());
         Events.onClick(buttonZoomOut, () => controls.zoomOut());
-        Events.onClick(buttonPanLeft, () => controls.panLeft());
         Events.onClick(buttonModePan, () => controls.setMouseMode(MouseMode.PAN_ONLY));
-        Events.onClick(buttonPanRight, () => controls.panRight());
-        Events.onClick(buttonRotateLeft, () => controls.rotateLeft());
-        Events.onClick(buttonRotateRight, () => controls.rotateRight());
-        Events.onClick(buttonRotateUp, () => controls.rotateUp());
-        Events.onClick(buttonRotateDown, () => controls.rotateDown());
         Events.onClick(buttonModeRotate, () => controls.setMouseMode(MouseMode.ROTATE_ONLY));
         Events.onClick(buttonCenter, () => controls.reset());
     }
