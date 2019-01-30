4d2809a60ab071e3a522fc7b103aca9ac95643f8
Krzysztof Mozejko <k.mozejko@ocado.com>
2019-01-30 23:30:17 +0100

    #18 white module color instead of black

--- src/model/colors.ts
+++ src/model/colors.ts
@@ -2,14 +2,14 @@ import {TexturesLibrary} from "../utils/textures";
 import {TextureDefinition} from "../utils/textures";
 
 export enum ColorType {
-    BLACK, GRAY, WHITE, WOOD
+    WHITE, GRAYLIGHT, GRAY, WOOD
 }
 
 export const ColorTypeUrls = [
-    new TextureDefinition(ColorType.BLACK, 'textures/black.jpg'),
+    new TextureDefinition(ColorType.WHITE, 'textures/white.jpg'),
+    new TextureDefinition(ColorType.GRAYLIGHT, 'textures/gray-light.jpg'),
     new TextureDefinition(ColorType.GRAY, 'textures/gray.jpg'),
     new TextureDefinition(ColorType.WOOD, 'textures/wood.jpg'),
-    new TextureDefinition(ColorType.WHITE, 'textures/gray-light.jpg')
 ];
 
 export class ColorTypeLibrary extends TexturesLibrary<ColorType> {
