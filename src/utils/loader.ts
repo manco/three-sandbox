81cf5faf16fdae30da7b0d1401928fc5a5938a47
Krzysztof Mozejko <k.mozejko@ocado.com>
2019-02-04 00:07:46 +0100

    fixes in poc'es, poc'es moved to static, server.js routing changed to serve poc'es if requested. Also, showcase poc done

--- src/utils/loader.ts
+++ src/utils/loader.ts
@@ -1,14 +1,13 @@
 import '../../node_modules/three/examples/js/loaders/OBJLoader';
-import {Mesh, Object3D} from "three";
+import {Mesh} from "three";
 import {Group} from "three";
-import {Lang} from "./lang";
 
 export class PromisingLoader {
     // @ts-ignore
     private readonly loader = new THREE.OBJLoader();
     loadSingleMesh(url:string) {
-        return new Promise<Object3D>(
-            (resolve) => this.loader.load(url, resolve, Lang.noop, Lang.noop)
+        return new Promise<Group>(
+            (resolve) => this.loader.load(url, resolve)
         ).then((obj:Group) => {
             if (obj.children.length > 1) {
                 console.warn(`loadSingleMesh: ${url} resolved to group of meshes: ${obj.children}`)
--- static/server.js
+++ static/server.js
@@ -5,9 +5,9 @@ const app = express();
 
 app.use(express.static(__dirname));
 
-// send the user to index html page inspite of the url
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'index.html'));
 });
+
 
 app.listen(port);
