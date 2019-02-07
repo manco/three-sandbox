ca8553b813fb00cf319603b056274955285e5c65
Krzysztof Mozejko <k.mozejko@ocado.com>
2019-02-07 10:30:10 +0100

    module front/color material referenced as fields, not methods

--- src/model/modules/module.ts
+++ src/model/modules/module.ts
@@ -5,7 +5,6 @@ import {MutateMeshFun} from "../../utils/meshes";
 import {ModuleType} from "./types";
 import {ModuleSubtype} from "./types";
 import {ModuleFunction} from "./module-functions";
-import {BufferGeometry} from "three";
 
 export class ModuleDefinition {
     constructor(
@@ -17,7 +16,8 @@ export class ModuleDefinition {
 
 export class Module {
     public readonly id: string = this.mesh.uuid;
-    private readonly hasFront = (this.mesh.geometry as BufferGeometry).groups.length > 0;
+    private readonly colorMaterial;
+    private readonly frontMaterial;
 
     constructor(
         readonly mesh: Mesh,
@@ -27,42 +27,33 @@ export class Module {
         readonly width: number,
         readonly depth: number,
         private readonly rotateFun: MutateMeshFun
-    ) { }
+    ) {
+        if (Array.isArray(mesh.material)) {
+            this.colorMaterial = mesh.material[1] as MeshLambertMaterial;
+            this.frontMaterial = mesh.material[0] as MeshLambertMaterial;
+        } else {
+            this.colorMaterial = this.frontMaterial = mesh.material as MeshLambertMaterial;
+        }
+    }
 
     initRotation(): void {
         this.rotateFun(this.mesh);
     }
 
     setColor(texture:Texture): void {
-        Module.setTexture(this.colorMaterial(), texture);
+        Module.setTexture(this.colorMaterial, texture);
     }
 
     getColor(): Texture {
-        return this.colorMaterial().map;
+        return this.colorMaterial.map;
     }
 
     getFrontTexture(): Texture {
-        return this.frontMaterial().map;
-    }
-
-    private colorMaterial() {
-        if (this.hasFront) {
-            //TODO material type definition
-            return this.mesh.material[1] as MeshLambertMaterial;
-        }
-        return this.mesh.material as MeshLambertMaterial;
-    }
-
-    private frontMaterial() {
-        if (this.hasFront) {
-            //TODO material type definition
-            return this.mesh.material[0] as MeshLambertMaterial;
-        }
-        throw "module has no front!";
+        return this.frontMaterial.map;
     }
 
     setFrontTexture(texture: Texture):void {
-        Module.setTexture(this.frontMaterial(), texture);
+        Module.setTexture(this.frontMaterial, texture);
     }
 
     private static setTexture(material: MeshLambertMaterial, texture: Texture) {
