b2873250b61510527edf4ced65e580bd0ff4f9f5
Krzysztof Mozejko <k.mozejko@ocado.com>
2019-04-14 11:12:26 +0200

    e2e test - assertion that kichen has standing sink

--- src/__tests__/e2e/tc.js
+++ src/__tests__/e2e/tc.js
@@ -1,40 +1,56 @@
 import { Selector } from 'testcafe';
+import { ClientFunction } from 'testcafe';
 
-fixture('TestCafe Example')
+fixture('Kuchnioplaner e2e')
     .page('http://localhost:9090');
 
-function modulesListTabletop() {
-    return Selector('#modulesList-1');
+function modulesListTabletopItem(id) {
+    return Selector('#modulesList-1').child('li').withAttribute('value', id);
 }
 
 test('Test1', async t => {
-    function setupKitchen() {
+
+    const kitchenHasStandingSink = ClientFunction(
+        () => {
+            const standing = kitchen.modules.byType(0);
+            const standingSink = standing.find(m => m.subtype === 4);
+            return standingSink !== undefined;
+        }
+    );
+
+    let setupKitchen = () => {
         return t
             .click('#checkbox-wallB')
             .click('#checkbox-wallC')
             .click('#drawKitchenButton');
     }
 
-    await
-        setupKitchen()
-        // set black hanging
-            .click('#modulesList-0-color')
+    let setBlackColorForModule = (moduleListId) => {
+        return t
+            .click(moduleListId)
             .click('#texture-2')
-            .click('#chooseColorModalClose')
+            .click('#chooseColorModalClose');
+    }
 
-        // set color on tabletop
-            .click('#modulesList-1-color')
-            .click('#texture-2')
-            .click('#chooseColorModalClose')
+    await setupKitchen();
+    await setBlackColorForModule('#modulesList-2-color') //hanging
+    await setBlackColorForModule('#modulesList-1-color') //tabletop
 
-        // set sink
+    //TODO extract method select subtype for module
+
+    // set sink
+    let selectModule1 = modulesListTabletopItem('65006')
+
+    await t.click(selectModule1)
             .click(
-                modulesListTabletop().child('li').withAttribute('value', '65006')
+                selectModule1.child('select').child('option').withAttribute('value', '1')
             )
 
-        // set oven-tabletop
+    let selectModule2 = modulesListTabletopItem('68000')
+    // set oven-tabletop
+    await t.click(selectModule2)
             .click(
-                modulesListTabletop().child('li').withAttribute('value', '68000')
+                selectModule2.child('select').child('option').withAttribute('value', '3')
             )
 
         //set standing color
@@ -43,7 +59,7 @@ test('Test1', async t => {
         // set washer
 
     //assert scene and kitchen
-
+            .expect(kitchenHasStandingSink()).ok()
         // assert that there is sink in standing
         // assert colors
         // assert textures set
