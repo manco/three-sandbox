73f8d5d44c8c52720a4b9be2240353b862c41dd9
Krzysztof Mozejko <k.mozejko@ocado.com>
2019-11-05 16:18:00 +0100

    refactor extract method

--- src/utils/textures.ts
+++ src/utils/textures.ts
@@ -1,6 +1,8 @@
 import {TextureLoader} from "three";
 import {Texture} from "three";
 import {RepeatWrapping} from "three";
+import {LinearMipMapLinearFilter} from "three";
+import {NearestFilter} from "three";
 
 export class TextureDefinition<T> {
     constructor(
@@ -20,10 +22,20 @@ export abstract class TexturesLibrary<T> {
             definitions.map(
             d =>
                 new Promise<Texture>(f => this.loader.load(d.url, f))
-                    .then(t => {t.name = d.type.toString(); t.wrapS = RepeatWrapping; t.wrapT = RepeatWrapping; return t;})
+                    .then(t => {
+                        t.name = d.type.toString();
+                        return TexturesLibrary.withOptions(t);
+                    })
                     .then(tex => this.textures.set(d.type, tex))
             )
         );
+    }
+
+
+    private static withOptions(t: Texture): Texture {
+        t.wrapS = RepeatWrapping;
+        t.wrapT = RepeatWrapping;
+        return t;
     }
 
     loadingPromise() {
